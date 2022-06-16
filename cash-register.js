a791df2fe147c1a7068f0a215a9336c5de5dbba0
jadashi97 <njadauniversal23@yahoo.com>
2022-06-16 10:44:28 -0500

    (feat):add some fuctionality and variable to start the cash register

--- cash-register.js
+++ cash-register.js
@@ -1,12 +1,37 @@
 function checkCashRegister(price, cash, cid) {
     let change = price - cash; //this is for change
 
+    let totalCash = 0; //var to hold total cash in register
+
+    let cidReverse = [...cid].reverse();
+    
+
+    let cashValue = { //object to show base currency unit
+        "PENNY": 0.01,
+        "NICKEL": 0.05,
+        "DIME": 0.1,
+        "QUARTER": 0.25,
+        "ONE": 1,
+        "FIVE": 5,
+        "TEN": 10,
+        "TWENTY": 20,
+        "ONE HUNDRED": 100
+    };
+
+    //object with status and change to be returned
     let changeDue = {
-        cash: Number,
-        price: Number
-    }
+        status : ' ',
+        change: [ ]
+    };
 
+    //get total cash in register
+    cid.forEach(element => {
+        totalCash += element[1];
+    });
+    totalCash = parseFloat(totalCash.toFixed(2)) //2 decimal places
+    console.log(totalCash);
 
+    //function to check if some currency unit can offset some change in the cash register
     return change;
 
 }
