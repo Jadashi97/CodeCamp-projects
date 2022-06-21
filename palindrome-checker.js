ac9cef2a6537b5a4a3e4ec97568866fc9457d14d
jadashi97 <njadauniversal23@yahoo.com>
2022-06-21 10:43:26 -0500

    (feat): completed palindrome checker project

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,7 +1,11 @@
 function palindrome(str) {
-    let str1 = str.toLowerCase().replace(/[^a-z\d]/g, " "); //switch the lowercase and replace any other non alpanumerics with an empty string
-    return str1.split('').reverse().join('');
-    
+    let str1 = str.toLowerCase().replace(/[^a-z\d]/g, ""); //switch the lowercase and replace any other non alpanumerics with an empty string
+    let reversedStr1 = str1.split('').reverse().join('');
+    if(str1 === reversedStr1){
+        return true
+    }else{
+        return false
+    }
 }
   
-console.log(palindrome("_BaBooon"));
+console.log(palindrome("eye"));
