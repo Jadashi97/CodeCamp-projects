98196e844434c90c6e476550177aad8fa6e8644b
jadashi97 <njadauniversal23@yahoo.com>
2022-06-21 10:44:09 -0500

    (feat): completed palindrome checker project

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,6 +1,6 @@
 function palindrome(str) {
     let str1 = str.toLowerCase().replace(/[^a-z\d]/g, ""); //switch the lowercase and replace any other non alpanumerics with an empty string
-    let reversedStr1 = str1.split('').reverse().join('');
+    let reversedStr1 = str1.split('').reverse().join(''); // instantiate another variable and split,reverse and join it
     if(str1 === reversedStr1){
         return true
     }else{
